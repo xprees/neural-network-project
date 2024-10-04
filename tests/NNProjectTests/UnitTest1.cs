26fcf10bcd5f4beba388d67ede8eeadee8261e0a
Vaclav Bily <xbily@fi.muni.cz>
2024-10-05 01:02:33 +0200

    Added basic ci pipeline and empty test project

---
+++ tests/NNProjectTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace NNProjectTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
