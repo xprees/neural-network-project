f8347862ca0efb1ce1ec2b684330b299710a96c5
Jan Maceček <robotik.888@seznam.cz>
2024-11-05 21:15:12 +0100

    feat: added basic DataLoader class

---
+++ src/DataLoading/DataLoader.cs
@@ -0,0 +1,41 @@
+﻿namespace DataLoading;
+
+public class DataLoader
+{
+    private const int RowSize = 28;
+    private const int BytesInRow = RowSize * RowSize;
+
+    private FileStream _fileStream;
+    private BinaryReader _reader;
+
+    public DataLoader(string path)
+    {
+        _fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
+
+        if (_fileStream == null)
+        {
+            throw new IOException("File could not be opened.");
+        }
+        else
+        {
+            _reader = new BinaryReader(_fileStream);
+        }
+    }
+
+    public byte[] GetPicBytes()
+    {
+        return _reader.ReadBytes(BytesInRow);
+    }
+
+    public byte[] GetAllBytes()
+    {
+        return _reader.ReadBytes((int) _fileStream.Length);
+    }
+
+    public void Close()
+    {
+        _reader.Close();
+        _fileStream.Close();
+    }
+    
+}
