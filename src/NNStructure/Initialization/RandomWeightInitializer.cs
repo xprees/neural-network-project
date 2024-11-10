a77d8f66bed43ec61406c818a4e1efbb7d0cb041
Vaclav Bily <xbily@fi.muni.cz>
2024-11-10 23:21:14 +0100

    RandomWeightInitializer refactor name

--- src/NNStructure/Initialization/RandomWeightInitializer.cs
+++ src/NNStructure/Initialization/RandomWeightInitializer.cs
@@ -2,7 +2,7 @@ using NNStructure.Layers;
 
 namespace NNStructure.Initialization;
 
-public class RandomValueInitializer(int seed = 42) : IWeightsInitializer
+public class RandomWeightInitializer(int seed = 42) : IWeightsInitializer
 {
     private readonly Random _random = new(seed);
 
