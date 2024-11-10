0261c8d1e910df696d6976e4a3e5095379009542
Vaclav Bily <xbily@fi.muni.cz>
2024-11-10 22:29:24 +0100

    RandomValueInitializer - added seed and using not thread safe variant

--- src/NNStructure/Initialization/RandomValueInitializer.cs
+++ src/NNStructure/Initialization/RandomValueInitializer.cs
@@ -2,9 +2,9 @@ using NNStructure.Layers;
 
 namespace NNStructure.Initialization;
 
-public class RandomValueInitializer() : IWeightsInitializer
+public class RandomValueInitializer(int seed = 42) : IWeightsInitializer
 {
-    private readonly Random _random = Random.Shared;
+    private readonly Random _random = new(seed);
 
     public float GetInitialWeight(ILayer initializedLayer) =>
         _random.NextSingle() - 0.5f;
