f3204545b2c8451336ab79a91dee6c19ad67351d
Vaclav Bily <xbily@fi.muni.cz>
2024-11-10 17:31:49 +0100

    Added IOptimizer

---
+++ src/NNStructure/Optimizers/IOptimizer.cs
@@ -0,0 +1,11 @@
+namespace NNStructure.Optimizers;
+
+/// Interface for all optimizers used in the Neural Network, e.g. Stochastic Gradient Descent, Adam, etc.
+/// They keep their state between the epochs and update the weights of the network such as  learning rate, momentum, etc.
+/// <remarks>If optimizer has inner state that changes make sure that accessing/updating it is thread-safe</remarks>
+public interface IOptimizer
+{
+    public float LearningRate { get; set; }
+
+    void UpdateWeights(float[][] weights, float[][] gradients, float learningRate);
+}
