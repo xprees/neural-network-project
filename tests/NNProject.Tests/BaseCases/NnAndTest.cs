0deb2c22017023a4c34e4412eeb6b3b8ec8dd46b
Vaclav Bily <xbily@fi.muni.cz>
2024-11-10 23:45:32 +0100

    And, XOR tests updated - not passing yet

--- tests/NNProject.Tests/BaseCases/NnAndTest.cs
+++ tests/NNProject.Tests/BaseCases/NnAndTest.cs
@@ -9,7 +9,7 @@ using NNStructure.Optimizers;
 namespace NNProjectTests.BaseCases;
 
 [TestFixture]
-public class NnXorTests
+public class NnAndTest
 {
     private NeuralNetwork _nn;
 
@@ -25,9 +25,9 @@ public class NnXorTests
 
     private static readonly float[][] ExpectedResults =
     [
+        [1],
         [0],
-        [1],
-        [1],
+        [0],
         [0]
     ];
 
@@ -36,7 +36,7 @@ public class NnXorTests
     [SetUp]
     public void Setup()
     {
-        _nn = new NeuralNetwork(new MeanSquaredError(), new RandomWeightInitializer(), new SgdOptimizer(0.05f));
+        _nn = new NeuralNetwork(new MeanSquaredError(), new RandomWeightInitializer(), new SgdOptimizer(0.005f));
         _nn.AddLayer(new FullyConnectedLayer(2, 2, new Relu()));
         _nn.AddLayer(new FullyConnectedLayer(2, 1, new Relu()));
     }
@@ -45,7 +45,7 @@ public class NnXorTests
     [TestCase(2, 5)]
     [TestCase(3, 5)]
     [TestCase(5, 5)]
-    public void TestNnXorTraining(int miniBatchSize, int maxEpochs)
+    public void TestNnAndTraining(int miniBatchSize, int maxEpochs)
     {
         _nn.InitializeWeights();
 
@@ -53,20 +53,20 @@ public class NnXorTests
 
         const float precision = 0.25f;
 
-        // 1, 1 = 0
+        // 1, 1 = 1
         var result = _nn.ForwardPropagate([1, 1]).First();
-        result.Should().BeApproximately(0, precision, $"For [1, 1] expected 0, got {result}");
+        result.Should().BeApproximately(1, precision, $"For [1, 1] expected 1, got {result}");
 
         // 0, 0 = 0
         result = _nn.ForwardPropagate([0, 0]).First();
         result.Should().BeApproximately(0, precision, $"For [0, 0] expected 0, got {result}");
 
-        // 1, 0 = 1
+        // 1, 0 = 0
         result = _nn.ForwardPropagate([1, 0]).First();
-        result.Should().BeApproximately(1, precision, $"For [1, 0] expected 1, got {result}");
+        result.Should().BeApproximately(0, precision, $"For [1, 0] expected 0, got {result}");
 
-        // 0, 1 = 1
+        // 0, 1 = 0
         result = _nn.ForwardPropagate([0, 1]).First();
-        result.Should().BeApproximately(1, precision, $"For [0, 1] expected 1, got {result}");
+        result.Should().BeApproximately(0, precision, $"For [0, 1] expected 0, got {result}");
     }
 }
