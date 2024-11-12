9ad12c8dfe0506732066c636a83fa0c09cea292c
Jan Maceček <robotik.888@seznam.cz>
2024-11-12 01:09:11 +0100

    fix: preprocessing normalization fixed

--- src/DataLoading/Preprocessing.cs
+++ src/DataLoading/Preprocessing.cs
@@ -5,7 +5,7 @@ public static class Preprocessing
     public static float[]?[] NormalizeByDivision(float[]?[] input)
     {
         return input
-            .Select(subArray => subArray?.Select(element => element / 256).ToArray())
+            .Select(subArray => subArray?.Select(element => element / 255).ToArray())
             .ToArray();
     }
 
