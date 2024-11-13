b44f3289711b8dc639d43e8c77a8d4d7f72e6606
Jan Maceček <robotik.888@seznam.cz>
2024-11-13 19:00:55 +0100

    feat: Incomplete evaluator added

---
+++ src/DataLoading/Evaluator.cs
@@ -0,0 +1,17 @@
+﻿namespace DataLoading;
+
+public class Evaluator(string path, float maxError = 0.0001f)
+{
+    private readonly DataLoader _resultLoader = new(path);
+
+    public bool EvaluateNext(float predictedResult)
+    {
+        var vector = _resultLoader.ReadOneVector();
+        if (vector == null)
+        {
+            throw new NullReferenceException("Read result is null");
+        }
+        return MathF.Abs(predictedResult - vector[0]) < maxError;
+
+    }
+}
