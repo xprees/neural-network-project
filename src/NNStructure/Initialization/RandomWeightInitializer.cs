2ece6dce33e557778cdd6c82e81b26b60521b380
Vaclav Bily <xbily@fi.muni.cz>
2024-11-13 17:41:43 +0100

    RandomWeightInitializer - making smaller weights closer to 0

--- src/NNStructure/Initialization/RandomWeightInitializer.cs
+++ src/NNStructure/Initialization/RandomWeightInitializer.cs
@@ -7,5 +7,5 @@ public class RandomWeightInitializer(int seed = 42) : IWeightsInitializer
     private readonly Random _random = new(seed);
 
     public float GetInitialWeight(ILayer initializedLayer) =>
-        _random.NextSingle() - 0.5f;
+        (_random.NextSingle() - 0.5f) * 0.1f;
 }
