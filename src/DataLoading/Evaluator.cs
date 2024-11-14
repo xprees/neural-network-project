207bf16bbb22bff6c5776c5cf170204ddd623b5d
Jan Maceček <robotik.888@seznam.cz>
2024-11-14 17:51:11 +0100

    feat: New evaluator added

--- src/DataLoading/Evaluator.cs
+++ src/DataLoading/Evaluator.cs
@@ -3,5 +3,63 @@
 public class Evaluator(float maxError = 0.0001f)
 {
 
+    public bool EvaluateCompleteResult(float[] predicted, float[] actual)
+    {
+        return predicted.
+            Select((value, index) => MathF.Abs(value - actual[index]) <= maxError)
+            .All(condition => condition);
+    }
+
+    public int[] ConvertToClasses(float[][] toConvert)
+    {
+        return toConvert.Select(value => Array.IndexOf(value, value.Max())).ToArray();
+    }
+
+    public bool[] EvaluateCompletedResults(float[][] predicted, float[][] actual)
+    {
+        return predicted.Select((value, index) => EvaluateCompleteResult(value, actual[index])).ToArray();
+    }
+
+    public StatisticalMetrics EvaluateModel(float[][] predicted, float[][] actual)
+    {
+        if (predicted.Length != actual.Length)
+        {
+            throw new ArgumentException("Predicted and actual must have the same length");
+        }
+        var statisticalMetrics = new StatisticalMetrics();
+        var convertedPredicted = ConvertToClasses(predicted);
+        var convertedActual = ConvertToClasses(actual);
+        for (int classIndex = 0; classIndex < 10; classIndex++) // Not the most effective, can be remade later
+        {
+            int fp = 0;
+            int fn = 0;
+            int tp = 0;
+            int tn = 0;
+
+            for (int arrayIndex = 0; arrayIndex < convertedPredicted.Length; arrayIndex++)
+            {
+  
+                if (convertedActual[arrayIndex] == classIndex && convertedPredicted[arrayIndex] == classIndex)
+                {
+                    tp += 1;
+                }  else if (convertedActual[arrayIndex] == classIndex && convertedPredicted[arrayIndex] != classIndex)
+                {
+                    fn += 1;
+                } else if (convertedActual[arrayIndex] != classIndex && convertedPredicted[arrayIndex] == classIndex)
+                {
+                    fp += 1;
+                }
+                else
+                {
+                    fp += 1;
+                }
+
+            }
+            statisticalMetrics.AssignConfusionMatrixCoefficients(classIndex, tp, tn, fp, fn);
+            
+        }
+
+        return statisticalMetrics;
+    }
 
 }
---
+++ src/DataLoading/StatisticalMetrics.cs
@@ -0,0 +1,45 @@
+﻿namespace DataLoading;
+
+public struct StatisticalMetrics
+{
+    public int[] TruePositives;
+    public int[] TrueNegatives;
+    public int[] FalsePositives;
+    public int[] FalseNegatives;
+
+    public float[] Precisions;
+    public float[] Accuracies;
+    public float[] Recalls;
+    public float[] F1Scores;
+
+    public float Precision;
+    public float Accuracy;
+    public float Recall;
+    public float F1Score;
+
+    public StatisticalMetrics()
+    {
+        TruePositives = new int[10];
+        TrueNegatives = new int[10];
+        FalsePositives = new int[10];
+        FalseNegatives = new int[10];
+        
+        Precisions = new float[10];
+        Accuracies = new float[10];
+        Recalls = new float[10];
+        F1Scores = new float[10];
+
+        Precision = 0;
+        Accuracy = 0;
+        Recall = 0;
+        F1Score = 0;
+    }
+
+    public void AssignConfusionMatrixCoefficients(int classIndex, int tp, int tn, int fp, int fn)
+    {
+        TruePositives[classIndex] = tp;
+        TrueNegatives[classIndex] = tn;
+        FalseNegatives[classIndex] = fn;
+        FalsePositives[classIndex] = fp;
+    }
+}
