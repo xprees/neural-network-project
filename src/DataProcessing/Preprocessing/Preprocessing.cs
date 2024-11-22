5d0ead7a18dd92e22e78db8b23076a9da48b3b14
Vaclav Bily <xbily@fi.muni.cz>
2024-11-22 18:12:57 +0100

    Preprocessing - add ShuffleData

--- src/DataProcessing/Preprocessing/Preprocessing.cs
+++ src/DataProcessing/Preprocessing/Preprocessing.cs
@@ -8,4 +8,27 @@ public class Preprocessing
             .Select(subArray => subArray.Select(element => element / 255).ToArray())
             .ToArray();
     }
+
+    public (float[][] trainInput, float[][] expectedOutput) ShuffleData(float[][] input, float[][] output,
+        int seed = 42)
+    {
+        if (input.Length != output.Length)
+        {
+            throw new ArgumentException("Input and output arrays must have the same length");
+        }
+
+        var random = new Random(seed);
+        var shuffledIndices = Enumerable.Range(0, input.Length).ToArray();
+        random.Shuffle(shuffledIndices);
+
+        var shuffledInput = new float[input.Length][];
+        var shuffledOutput = new float[output.Length][];
+        foreach (var index in shuffledIndices)
+        {
+            shuffledInput[index] = input[index];
+            shuffledOutput[index] = output[index];
+        }
+
+        return (shuffledInput, shuffledOutput);
+    }
 }
