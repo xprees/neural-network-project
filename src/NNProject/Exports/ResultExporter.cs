722c497979f9a5d4c753e841f229b820e2540892
Vaclav Bily <xbily@fi.muni.cz>
2024-11-22 21:55:12 +0100

    Added ResultExporter

---
+++ src/NNProject/Exports/ResultExporter.cs
@@ -0,0 +1,13 @@
+namespace NNProject.Exports;
+
+public static class ResultExporter
+{
+    public static async Task ExportResultsAsCsvAsync<T>(string filePath, IEnumerable<T> results)
+    {
+        await using var writer = new StreamWriter(filePath);
+        foreach (var result in results)
+        {
+            await writer.WriteLineAsync($"{result}{Environment.NewLine}");
+        }
+    }
+}
