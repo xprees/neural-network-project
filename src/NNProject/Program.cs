a7bec1ab7a80dce3b012b83cd1479410f2838f41
Vaclav Bily <xbily@fi.muni.cz>
2024-11-22 20:15:21 +0100

    Program - add draft of the NN for Fashion Mnist

--- src/NNProject/Program.cs
+++ src/NNProject/Program.cs
@@ -1,4 +1,7 @@
+using DataProcessing.Encoding;
+using DataProcessing.Evaluation;
 using DataProcessing.Loading;
+using DataProcessing.Preprocessing;
 using NNProject;
 using NNStructure;
 using NNStructure.ActivationFunctions;
@@ -8,42 +11,73 @@ using NNStructure.LossFunctions;
 using NNStructure.Optimizers;
 
 // Load the data
-string? trainDataPath = null;
-if (args.Length > 0) trainDataPath = args[0];
+var (trainDataPath, trainLabelsPath, testDataPath, testLabelsPath) = GetDatasetFilesPaths();
 
-trainDataPath ??= DatasetPathFinder.GetTestVectorsPath()
-                  ?? throw new ArgumentException("No path provided and no default path found");
+using var trainDataLoader = new DataLoader(trainDataPath);
+var trainData = trainDataLoader.ReadAllVectors();
 
-using var dataLoader = new DataLoader(trainDataPath);
-
+using var trainLabelsLoader = new DataLoader(trainLabelsPath);
+var trainLabels = trainLabelsLoader.ReadAllVectors();
 
 // Preprocess the data
 
+var preprocessing = new Preprocessing();
+var normalizedData = preprocessing.NormalizeByDivision(trainData);
+
+var oneHotEncoder = new OneHotEncoder<int>(Enumerable.Range(0, 10));
+
+var trainLabelsOneHot = oneHotEncoder.Encode(trainLabels.Select(x => (int)x.First()));
+
+var (trainInput, trainingExpectedOutput) = preprocessing.ShuffleData(normalizedData, trainLabelsOneHot.ToArray());
+
 // Create the neural network
-var nn = new NeuralNetwork(new MeanSquaredError(), new GlorotWeightInitializer(), new SgdOptimizer(0.8f));
-nn.AddLayer(new FullyConnectedLayer(2, 2, new Tanh()));
-nn.AddLayer(new FullyConnectedLayer(2, 1, new Tanh()));
+var nn = new NeuralNetwork(new MeanSquaredError(), new GlorotWeightInitializer(), new SgdOptimizer(0.5f));
+nn.AddLayer(new FullyConnectedLayer(784, 256, new Relu()));
+nn.AddLayer(new FullyConnectedLayer(256, 64, new Relu()));
+nn.AddLayer(new FullyConnectedLayer(64, 10, new Relu())); // TODO: Change to Softmax
 
 nn.InitializeWeights();
 
-float[][] inputs =
-[
-    [1, 1],
-    [1, 0],
-    [0, 1],
-    [0, 0]
-];
+nn.Train(trainInput, trainingExpectedOutput, 10, 256);
 
-float[][] expectedResults =
-[
-    [0],
-    [1],
-    [1],
-    [0]
-];
+using var testDataLoader = new DataLoader(testDataPath);
+var testData = testDataLoader.ReadAllVectors();
 
-nn.Train(inputs, expectedResults, 100, 4);
+using var testLabelsLoader = new DataLoader(testLabelsPath);
+var testLabels = testLabelsLoader.ReadAllVectors();
 
-var result = nn.ForwardPropagate([1, 1]).prediction.FirstOrDefault();
+var result = nn.Test(testData);
 
-Console.WriteLine($"For [1, 1] expected 0, got {result}");
+var accuracyEvaluator = new AccuracyEvaluator();
+var accuracy = accuracyEvaluator.Evaluate(result, testLabels);
+
+Console.WriteLine($"Accuracy: {accuracy: 2f}");
+return;
+
+// Args: trainDataPath trainLabelsPath testDataPath testLabelsPath
+(string trainDataPath, string trainLabelsPath, string testDataPath, string testLabelsPath) GetDatasetFilesPaths()
+{
+    string? trainDataPath = null;
+    var hasArgumentsWithDataPaths = args.Length >= 4;
+    if (hasArgumentsWithDataPaths) trainDataPath = args[0];
+
+    trainDataPath ??= DatasetPathFinder.GetTrainVectorsPath()
+                      ?? throw new ArgumentException("No path for trainData provided and no default path found");
+
+    string? trainLabelsPath = null;
+    if (hasArgumentsWithDataPaths) trainLabelsPath = args[1];
+    trainLabelsPath ??= DatasetPathFinder.GetTrainLabelsPath()
+                        ?? throw new ArgumentException("No path for trainLabels provided and no default path found");
+
+    string? testDataPath = null;
+    if (hasArgumentsWithDataPaths) testDataPath = args[2];
+    testDataPath ??= DatasetPathFinder.GetTestVectorsPath()
+                     ?? throw new ArgumentException("No path for testData provided and no default path found");
+
+    string? testLabelsPath = null;
+    if (hasArgumentsWithDataPaths) testLabelsPath = args[3];
+    testLabelsPath ??= DatasetPathFinder.GetTestLabelsPath()
+                       ?? throw new ArgumentException("No path for testLabels provided and no default path found");
+
+    return (trainDataPath, trainLabelsPath, testDataPath, testLabelsPath);
+}
