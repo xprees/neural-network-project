4ba3d115793dcad37658cc91bb46f99c4c629cd0
Vaclav Bily <xbily@fi.muni.cz>
2024-11-24 00:28:33 +0100

    ResultExporter add sync method

--- src/NNProject/Exports/ResultExporter.cs
+++ src/NNProject/Exports/ResultExporter.cs
@@ -10,4 +10,13 @@ public static class ResultExporter
             await writer.WriteLineAsync($"{result}");
         }
     }
+
+    public static void ExportResultsAsCsv<T>(string filePath, IEnumerable<T> results)
+    {
+        using var writer = new StreamWriter(filePath);
+        foreach (var result in results)
+        {
+            writer.WriteLine($"{result}");
+        }
+    }
 }
