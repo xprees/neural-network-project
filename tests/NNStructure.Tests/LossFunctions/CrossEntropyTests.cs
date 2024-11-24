e0bd05e5873e9e0c5960f18f37b609200fd3bb04
Jan Maceček <robotik.888@seznam.cz>
2024-11-24 19:30:17 +0100

    fix: crossEntropy is not INF

--- tests/NNStructure.Tests/LossFunctions/CrossEntropyTests.cs
+++ tests/NNStructure.Tests/LossFunctions/CrossEntropyTests.cs
@@ -8,11 +8,30 @@ public class CrossEntropyTests
     [Test]
     public void SoftMaxTestSum()
     {
-        float[] predicted = [-2f ,-1f , -0.1f, 100f, -100f, -0f, 0f, 0.000001f, -0.000001f, -0.000001f];
+        float[] predicted = [-2f ,-1f , -0.1f, 1000f, -100f, -0f, 0f, 0.000001f, -0.000001f, -0.000001f];
         CrossEntropy entropy = new CrossEntropy();
         predicted = entropy.ComputeSoftMaxOnVector(predicted);
         predicted.Sum().Should().BeApproximately(1.0f, 0.000001f);
         Console.WriteLine(predicted.Sum() + " was predicted... ");
+        for (int i = 0; i < predicted.Length; i++)
+        {
+            Console.WriteLine(predicted[i] + " was " + predicted[i]);
+        }
+
+    }
+    
+    [Test]
+    public void CrossEntropyTest()
+    {
+        float[] predicted = [-2f ,-1f , -0.1f, 100f, -100f, -0f, 0f, 0.000001f, -0.000001f, -0.000001f];
+        CrossEntropy entropy = new CrossEntropy();
+        for (int i = 0; i < predicted.Length; i++)
+        {
+            float[] expected = new float[predicted.Length];
+            float crossEntropy = entropy.CrossEntropyVector(predicted, i);
+            Console.WriteLine(i + ". " + crossEntropy + " was predicted... ");
+        }
+        
 
     }
 }
