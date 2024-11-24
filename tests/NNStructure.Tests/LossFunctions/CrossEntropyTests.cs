93bed950c10a22ecc8368c1e5586ecc05eebf9f1
Jan Maceček <robotik.888@seznam.cz>
2024-11-24 18:59:36 +0100

    fix: stabilized SoftMax added

---
+++ tests/NNStructure.Tests/LossFunctions/CrossEntropyTests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using NNStructure.CrossEntropy;
+
+namespace NNStructureTests.LossFunctions;
+
+public class CrossEntropyTests
+{
+    [Test]
+    public void SoftMaxTestSum()
+    {
+        float[] predicted = [-2f ,-1f , -0.1f, 100f, -100f, -0f, 0f, 0.000001f, -0.000001f, -0.000001f];
+        CrossEntropy entropy = new CrossEntropy();
+        predicted = entropy.ComputeSoftMaxOnVector(predicted);
+        predicted.Sum().Should().BeApproximately(1.0f, 0.000001f);
+        Console.WriteLine(predicted.Sum() + " was predicted... ");
+
+    }
+}
