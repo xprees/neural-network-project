edd60ae4f928af079e1b74e420953762d7af133e
Jan Maceček <robotik.888@seznam.cz>
2024-11-24 18:20:38 +0100

    feat: CrossEntropy implements ILossFunction

---
+++ src/NNStructure/LossFunctions/CrossEntropy.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel.DataAnnotations;
+using NNStructure.LossFunctions;
+
+namespace NNStructure.CrossEntropy;
+
+public class CrossEntropy: ILossFunction
+{
+    
+    private float ComputeCrossEntropyOnVector(float[] predictedVector, int actual)
+    {
+        return - MathF.Log(predictedVector[actual]);
+    }
+    
+    
+    private float[] ComputeSoftMaxOnVector(float[] predictedVector)
+    {
+        float[] softMax = predictedVector.Select(x => (float)Math.Exp(x)).ToArray();
+        float sum = softMax.Sum();
+        return softMax.Select(x => x/sum).ToArray();
+    }
+    
+    
+    // Applies SoftMax on predicted vector and computes CrossEntropy
+    private float CrossEntropyVector(float[] predictedVector, int actual)
+    {
+        return ComputeCrossEntropyOnVector(ComputeSoftMaxOnVector(predictedVector), actual);
+    }
+    
+    
+
+    public float Calculate(float[] predicted, float[] expected)
+    {
+        if (predicted.Length != expected.Length)
+        {
+            throw new ArgumentException("The length of predicted and expected arrays must be the same.");
+        }
+
+        if (predicted.Length <= 0) return 0;
+        
+        int index = expected.ToList().IndexOf(expected.Max());
+        return CrossEntropyVector(predicted, index);
+    }
+    
+
+    public float[] CalculateGradient(float[] predicted, float[] expected)
+    {
+        if (predicted.Length != expected.Length)
+        {
+            throw new ArgumentException("The length of predicted and expected arrays must be the same.");
+        }
+
+        var gradients = ComputeSoftMaxOnVector(predicted);
+        for (int i = 0; i < gradients.Length; i++)
+        {
+            gradients[i] -= expected[i];
+        }
+
+        return gradients;
+    }
+}
