fe1ae9e52aa44ec72366c9d00dfeaa217c8f537a
Jan Maceček <robotik.888@seznam.cz>
2024-11-24 17:33:41 +0100

    feat first try - softmax & crossEntropy

---
+++ src/NNStructure/CrossEntropy/CrossEntropy.cs
@@ -0,0 +1,31 @@
+﻿namespace NNStructure.CrossEntropy;
+
+public class CrossEntropy
+{
+    public static float ComputeCrossEntropyOnVector(float[] predictedVector, int actual)
+    {
+        return - MathF.Log(predictedVector[actual]);
+    }
+
+    public static float ComputeCrossEntropyOnBatch(float[][] predictedVectors, int[] actual, int batchSize)
+    {
+        float result = 0f;
+        for (int index = 0; index < batchSize; index++)
+        {
+            result += ComputeCrossEntropyOnVector(predictedVectors[index], actual[index]);
+        }
+        return result / batchSize;
+    }
+
+    public static float[] ComputeSoftMaxOnVector(float[] predictedVector)
+    {
+        float[] softMax = predictedVector.Select(x => (float)Math.Exp(x)).ToArray();
+        float sum = softMax.Sum();
+        return softMax.Select(x => x/sum).ToArray();
+    }
+    
+    public static float[][] ComputeSoftMaxOnBatch(float[][] predictedVectors)
+    {
+        return predictedVectors.Select(ComputeSoftMaxOnVector).ToArray();
+    }
+}
