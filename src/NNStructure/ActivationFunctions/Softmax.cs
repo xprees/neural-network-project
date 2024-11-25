2f3022ab05f3f4be2fec3b7ccbb40cec8bc5690b
Vaclav Bily <xbily@fi.muni.cz>
2024-11-25 09:28:47 +0100

    Softmax - small performance optimization caching max

--- src/NNStructure/ActivationFunctions/Softmax.cs
+++ src/NNStructure/ActivationFunctions/Softmax.cs
@@ -4,8 +4,9 @@ public class Softmax : IActivationFunction
 {
     public float[] ActivateLayer(float[] potentials)
     {
+        var max = potentials.Max();
         var softMax = potentials
-            .Select(x => x - potentials.Max()) // Subtracting the maximum value to prevent overflow
+            .Select(x => x - max) // Subtracting the maximum value to prevent overflow
             .Select(MathF.Exp)
             .ToArray();
         var sum = softMax.Sum();
