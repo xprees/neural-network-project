0bfa9657743340b9590dac3fb142ca869f71ea6a
Vaclav Bily <xbily@fi.muni.cz>
2024-11-29 15:48:18 +0100

    NnRunLog improved logging

--- src/NNProject/Logging/NnRunLog.cs
+++ src/NNProject/Logging/NnRunLog.cs
@@ -35,10 +35,11 @@ public record NnRunLog(List<NnEpochLog> EpochLogs, MnistNnOptions Options)
     public override string ToString() =>
         "================ RUN ===============\n" +
         $"Parameters: {Options}\n" +
-        $"Total Time Took: {TotalTimeTook} ms\n" +
-        $"Average Accuracy: {AverageAccuracy * 100:F2}%\n" +
         $"Best Accuracy: {BestAccuracy * 100:F2}%\n" +
+        $"Total time: {TotalTimeTook / 1000 / 60:F2} mins\n" +
+        $"Average Accuracy: {AverageAccuracy * 100:F2}%\n" +
         $"Min Accuracy: {MinAccuracy * 100:F2}%\n" +
+        "=====================================\n" +
         $"Final Metrics: {FinalMetrics}\n" +
         "=====================================\n" +
         "Epoch Logs:\n" +
