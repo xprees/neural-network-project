35cac798aa925da700a3a61eabb735190f412684
Vaclav Bily <xbily@fi.muni.cz>
2025-01-10 09:39:05 +0100

    CrossEntropy simplified

--- src/NNStructure/LossFunctions/CrossEntropy.cs
+++ src/NNStructure/LossFunctions/CrossEntropy.cs
@@ -1,42 +1,22 @@
-﻿using NNStructure.ActivationFunctions;
-
-namespace NNStructure.LossFunctions;
+﻿namespace NNStructure.LossFunctions;
 
 public class CrossEntropy : ILossFunction
 {
-    private const float Epsilon = 0.00000001f; // To avoid log(0)
-    private readonly Softmax _softmax = new();
-
-    public float ComputeCrossEntropyOnVector(float[] predictedVector, int actual) =>
-        -MathF.Log(predictedVector[actual] + Epsilon);
-
-    /// Applies SoftMax on predicted vector and computes CrossEntropy
-    public float CrossEntropyVector(float[] predictedVector, int actual) =>
-        ComputeCrossEntropyOnVector(_softmax.ActivateLayer(predictedVector), actual);
-
+    private const float Epsilon = 1e-8f; // To avoid log(0)
 
     public float Calculate(float[] predicted, float[] expected)
     {
-        if (predicted.Length != expected.Length)
+        float loss = 0;
+        for (var i = 0; i < predicted.Length; i++)
         {
-            throw new ArgumentException("The length of predicted and expected arrays must be the same.");
+            loss -= expected[i] * MathF.Log(predicted[i] + Epsilon);
         }
 
-        if (predicted.Length <= 0) return 0;
-
-        var max = expected.Max();
-        var index = expected.ToList().IndexOf(max);
-        return CrossEntropyVector(predicted, index);
+        return loss;
     }
-
 
     public float[] CalculateGradient(float[] predicted, float[] expected)
     {
-        if (predicted.Length != expected.Length)
-        {
-            throw new ArgumentException("The length of predicted and expected arrays must be the same.");
-        }
-
         // Softmax activation function is used in the output layer (predicted values were passed through it)
         var gradient = new float[predicted.Length];
         for (var i = 0; i < predicted.Length; i++)
--- tests/NNStructure.Tests/LossFunctions/CrossEntropyTests.cs
+++ tests/NNStructure.Tests/LossFunctions/CrossEntropyTests.cs
@@ -7,29 +7,6 @@ namespace NNStructureTests.LossFunctions;
 public class CrossEntropyTests
 {
     [Test]
-    public void CrossEntropyTest()
-    {
-        float[] predicted = [-2f, -1f, -0.1f, 10f, -10f, -0f, 0f, 0.000001f, -0.000001f, -0.000001f];
-        var entropy = new CrossEntropy();
-
-        var entropies = new float[10];
-        var minEntropy = 1000f;
-
-        for (var i = 0; i < predicted.Length; i++)
-        {
-            var crossEntropy = entropy.CrossEntropyVector(predicted, i);
-            Console.WriteLine($"{i}. {crossEntropy} was predicted... ");
-            entropies[i] = crossEntropy;
-            if (crossEntropy < minEntropy)
-            {
-                minEntropy = crossEntropy;
-            }
-        }
-
-        entropies[3].Should().Be(minEntropy);
-    }
-
-    [Test]
     public void CalculateGradientTest()
     {
         float[] predicted = [0.1f, 0.2f, 0.7f];
